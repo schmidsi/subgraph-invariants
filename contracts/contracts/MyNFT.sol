e184bbaa5eba88164aa8272c14e746823c159ead
Simon Emanuel Schmid <simon@schmid.io>
2023-01-26 09:38:18 +0100

    Add a bug to the contract

--- contracts/contracts/MyNFT.sol
+++ contracts/contracts/MyNFT.sol
@@ -2,11 +2,21 @@ pragma solidity ^0.8.4;
 
 import 'erc721a/contracts/ERC721A.sol';
 
+/*
+Note:
+This is a purposefully buggy NFT contract.
+DO NOT USE THIS IN PRODUCTION.
+*/
+
 contract MyNFT is ERC721A {
     constructor() ERC721A('My NFT', 'MYNFT') {}
 
     function mint(uint256 quantity) external {
-        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
         _mint(msg.sender, quantity);
+    }
+
+    function balanceOf(address owner) public view override returns (uint256) {
+        // Stupid but possible
+        return 0;
     }
 }
--- contracts/test/MyNFT.ts
+++ contracts/test/MyNFT.ts
@@ -1,22 +1,33 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
-describe("MyNFT", function () {
+describe('MyNFT', function() {
   async function deployFixture() {
     const [alice, bob] = await ethers.getSigners();
 
-    const MyNFT = await ethers.getContractFactory("MyNFT");
+    const MyNFT = await ethers.getContractFactory('MyNFT');
     const myNFT = await MyNFT.deploy();
 
     return { myNFT, alice, bob };
   }
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
+  describe('Deployment', function() {
+    it('should start with totalSupply == 0', async function() {
       const { myNFT } = await loadFixture(deployFixture);
 
       expect(await myNFT.totalSupply()).to.equal(0);
+    });
+
+    it('can mint multiple', async function() {
+      const { myNFT, alice } = await loadFixture(deployFixture);
+
+      await expect(myNFT.mint(2)).to.emit(myNFT, 'Transfer');
+
+      expect(await myNFT.totalSupply()).to.equal(2);
+
+      // This is the bug in the contract
+      expect(await myNFT.balanceOf(alice.address)).to.equal(0);
     });
   });
 });
