0aecfbe2dc23dad338baea4a8db4926970b6448e
Simon Emanuel Schmid <simon@schmid.io>
2023-01-26 09:26:44 +0100

    Use ERC721A

--- contracts/contracts/MyNFT.sol
+++ contracts/contracts/MyNFT.sol
@@ -1,22 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import 'erc721a/contracts/ERC721A.sol';
 
-// Import this file to use console.log
-// import "hardhat/console.sol";
+contract MyNFT is ERC721A {
+    constructor() ERC721A('My NFT', 'MYNFT') {}
 
-contract MyNFT is ERC721 {
-    uint256 private _totalSupply = 0;
-
-    constructor() ERC721("My NFT", "MYNFT") {}
-
-    function mint(address to) public {
-        _safeMint(to, totalSupply());
-        _totalSupply = totalSupply() + 1;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
+    function mint(uint256 quantity) external {
+        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
+        _mint(msg.sender, quantity);
     }
 }
--- contracts/tasks/mint.ts
+++ contracts/tasks/mint.ts
@@ -1,19 +1,19 @@
-import { task } from "hardhat/config";
-import networks from "subgraph/networks.json";
+import { task } from 'hardhat/config';
+import networks from 'subgraph/networks.json';
 
-task("mint", "Deploys the passed contract")
-  .addOptionalVariadicPositionalParam("to", "Receiver")
+task('mint', 'Deploys the passed contract')
+  .addOptionalVariadicPositionalParam('to', 'Receiver')
   .setAction(async ({ to }, hre) => {
-    const MyNFT = await hre.ethers.getContractFactory("MyNFT");
+    const MyNFT = await hre.ethers.getContractFactory('MyNFT');
 
     const mintTo = to || (await hre.ethers.getSigners())[0].address;
 
     const myNFT = MyNFT.attach(
-      (networks as any)[hre.network.name].MyNFT.address
+      (networks as any)[hre.network.name].MyNFT.address,
     );
 
-    const tx = await myNFT.mint(mintTo);
+    const tx = await myNFT.mint(2);
     const receipt = await tx.wait();
 
-    console.log(`Minted 1 NFT to ${mintTo}`);
+    console.log(`Minted ${2} NFTs to ${mintTo}`);
   });
